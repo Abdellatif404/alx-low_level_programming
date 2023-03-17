e4ae339036ab3268be7734ee1be5996ed0f55526
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-17 05:04:45 -0700

    Add last digit exercise

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,15 +12,18 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	last_digit = n % 10;
+	printf("Last digit of %d is %d ", n, last_digit);
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
 	else
-		printf("%d is zero\n", n);
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
