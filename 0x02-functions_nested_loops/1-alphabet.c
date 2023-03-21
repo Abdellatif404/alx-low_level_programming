0af7287578afa9c703a360f6b9969d968afaaa2c
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-21 13:47:13 -0700

    Solve task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	i = 97;
+	while (i <= 122)
+		_putchar(i++);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
-void _putchar(char * str)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	int i;
-
-	i = 0;
-	while (str[i])
-		putchar(str[i++]);
+	return (write(1, &c, 1));
 }
