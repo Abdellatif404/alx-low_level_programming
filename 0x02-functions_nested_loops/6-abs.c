4794693eeee25e84a1b7f255518c43976007b883
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-21 17:11:01 -0700

    Solve task 5, 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - check the code.
+ *
+ * Description - 'the program's description'
+ * @n: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
