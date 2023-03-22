92c6363d536c13596ef7ac6d7d3cf829607b6102
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-22 15:00:17 -0700

    Solve task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check the code.
+ *
+ * Description - 'the program's description'
+ * @a: describe the parameter
+ * @b: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
