820fc1ccaa0fe204658ac4a646bb483a6e6d95b8
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-22 13:27:22 -0700

    Solve debugging task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,10 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		return (n % 10 * -1 + 48);
-	return (n % 10 + 48);
+	int last_digit;
+
+	last_digit = n % 10;
+	if (n >= 0)
+		_putchar(last_digit + 48);
+	return (last_digit);
 }
