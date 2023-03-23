22633bafe179b7eaf039423ea48fe443551d7a6e
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-23 15:30:34 -0700

    Solve task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code.
+ *
+ * Description - 'the program's description'
+ * @size: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int size)
+{
+	int i, j, spaces;
+
+	i = 1;
+	spaces = size;
+	while (i <= size)
+	{
+		j = 0;
+		while (j < spaces)
+		{
+			_putchar(' ');
+			j++;
+		}
+		spaces--;
+		j = 0;
+		while (j < i)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
