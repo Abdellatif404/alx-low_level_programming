c69b975804a8ce7234012d361d3d5ded910b59dc
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-27 04:50:48 -0700

    Solve task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - check the code.
+ *
+ * Description - 'the program's description'
+ * @str: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		_putchar(str[i++];
+	_putchar('\n');
+}
