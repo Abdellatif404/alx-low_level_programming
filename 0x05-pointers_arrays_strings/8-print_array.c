78bbc0a72f5be89a03e726df7f31fc59eede4d9c
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-27 15:04:43 -0700

    Solve task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - check the code.
+ *
+ * Description - 'the program's description'
+ * @a: describe the parameter
+ * @n: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	
+	i = 0;
+	while (i < n)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+		i++;
+	}
+}
