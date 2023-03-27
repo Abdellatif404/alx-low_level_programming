12ab626120f9b2bf3f6b1f4128502a9e84599c3f
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-27 15:07:14 -0700

    Fix error

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,14 +13,15 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	i = 0;
 	while (i < n)
 	{
 		if (i != n - 1)
 			printf("%d, ", a[i]);
 		else
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 		i++;
 	}
+	_putchar('\n');
 }
