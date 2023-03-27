e943b2de8b293fd296c2ae4fdda395f2d442505f
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-27 04:02:57 -0700

    Solve task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_square - writes the character c to stdout
+ * @size: The size of square
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
