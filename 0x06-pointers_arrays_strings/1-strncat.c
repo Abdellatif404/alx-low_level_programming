4ddf62f415547d9357e5a7c431c22975ed4032d6
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-30 06:45:20 -0700

    Solve task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - check the code.
+ *
+ * Description - 'the program's description'
+ * @dest: describe the parameter
+ * @src: describe the parameter
+ * @n: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = src[j];
+	return (dest);
+}
