43aebce609cad7c66a3ce9ab3856b8e0158ad801
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-30 17:34:57 -0700

    Solve task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - check the code.
+ *
+ * Description - 'the program's description'
+ * @s: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+				s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+				s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+				s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}'
+				)
+		{
+			i++;
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
