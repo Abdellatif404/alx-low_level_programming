d71f443cf88524ac825352c8ecd7cfdc34396081
Abdellatif404 <elbouziadyapps@gmail.com>
2023-03-31 16:52:22 -0700

    Solve bonus task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - check the code.
+ *
+ * Description - 'the program's description'
+ * @n: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		print_number(n * -1);
+	}
+	else if (n < 10)
+		_putchar(n);
+	else
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+}
