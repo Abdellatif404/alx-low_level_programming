dc798066c1260bd6f3a8bfb88eeae7eb9cea3634
Abdellatif404 <elbouziadyapps@gmail.com>
2023-04-13 14:49:43 -0700

    Create string_nconcat function

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings.
+ *
+ * @s1: the given string
+ * @s2: the given string
+ * @n: the number of bytes to be copied
+ *
+ * Return: a pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+	char *str;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	i = 0;
+	while (s1[i])
+		i++;
+	while (s2[i])
+		i++;
+	str = malloc((i + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	i = j = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] && j < n)
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
