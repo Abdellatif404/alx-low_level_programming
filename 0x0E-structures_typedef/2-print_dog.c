52716a9734a830ab335c8063fcef2b7204de09d6
Abdellatif404 <elbouziadyapps@gmail.com>
2023-04-18 16:37:39 -0700

    Create print_dog function

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print dog struct
+ *
+ * @d: input pointer to struct dog
+ *
+ * Return: nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age)
+			printf("Age: %d\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
