46becb32b2991e2741ef0cfbb6be3f9c1bce72d9
Abdellatif404 <elbouziadyapps@gmail.com>
2023-04-18 17:34:48 -0700

    Quick fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,26 @@
 #include "dog.h"
 
 /**
+ * _strlen - check the code.
+ *
+ * Description - 'the program's description'
+ * @s: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int i, len;
+
+	i = len = 0;
+	while (s[i])
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+/**
  * new_dog - create a new dog
  *
  * @name: First field
@@ -13,17 +33,16 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *name_cpy, *owner_cpy;
 
-	name_cpy = name;
-	owner_cpy = owner;
 	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (0);
-	if (name)
-		d->name = name;
+	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (!d->name)
+		return (0);
 	d->age = age;
-	if (owner)
-		d->owner = owner;
+	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (!d->owner)
+		return (0);
 	return (d);
 }
