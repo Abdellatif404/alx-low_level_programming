5e613586b3815f6c7ff5a95adeb9c50fda074a1b
Abdellatif404 <elbouziadyapps@gmail.com>
2023-04-21 17:51:03 -0700

    Create print_name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - print a name.
+ *
+ * @name: name to print
+ * @f: function pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
