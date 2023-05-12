027e3dd283d1b05309131064ec93e3cd6c865652
Abdellatif404 <elbouziadyapps@gmail.com>
2023-05-12 06:58:52 -0700

    Quick fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (text_content)
-	{
-		len = 0;
-		while (text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
-	}
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 	if (fd == -1 || wr == -1)
