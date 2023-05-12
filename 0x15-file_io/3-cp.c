27a7d44046e2ee02eaa79d958e986ffb5a9d61eb
Abdellatif404 <elbouziadyapps@gmail.com>
2023-05-12 07:58:24 -0700

    Solve task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+/**
+ * create_buffer - Allocates 1024 bytes for buffer
+ * @file: the name of the file
+ * Return: a pointer to the allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ * close_file -  close a file
+ * @fd: the file descriptor
+ * Return: nothing.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @ac: the number of arguments
+ * @av: array of arguments
+ * Return: 0 on success.
+ */
+int main(int ac, char *av[])
+{
+	int from, to, rd, wr;
+	char *buffer;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = create_buffer(av[2]);
+	from = open(av[1], O_RDONLY);
+	rd = read(from, buffer, 1024);
+	to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
+	do {
+		if (from == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			free(buffer);
+			exit(98);
+		}
+		wr = write(to, buffer, rd);
+		if (to == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			free(buffer);
+			exit(99);
+		}
+		rd = read(from, buffer, 1024);
+		to = open(av[2], O_WRONLY | O_APPEND);
+	} while (rd > 0);
+	free(buffer);
+	close_file(from);
+	close_file(to);
+	return (0);
+}
