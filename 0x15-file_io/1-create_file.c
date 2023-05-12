536bfe7b1430bdf95daca38aa904e826dd577c45
Abdellatif404 <elbouziadyapps@gmail.com>
2023-05-12 06:54:21 -0700

    Solve task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the given file
+ * @text_content: string to write to the file
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int len, fd, wr;
+
+	if (!filename)
+		return (-1);
+	if (text_content)
+	{
+		len = 0;
+		while (text_content[len])
+			len++;
+	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+	if (fd == -1 || wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
