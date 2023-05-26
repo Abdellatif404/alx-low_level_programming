3a0e0aec76a63e2e6211690f5ad46828611680ab
Abdellatif404 <elbouziadyapps@gmail.com>
2023-05-26 15:41:51 -0700

    Solve task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - count the number of bits you would need to flip to get from one number to another
+ * @n: the first number
+ * @m: the second number
+ * Return: returns the number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int bit, shift = n ^ m;
+	int i, c = 0;
+
+	i = 63;
+	while (i >= 0)
+	{
+		bit = shift >> i;
+		if (bit & 1)
+			c++;
+		i--;
+	}
+	return (c);
+}
